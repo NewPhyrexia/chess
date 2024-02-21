f6ac2f065dd613acf415d4b49d292d5a0c9d0c43
wajih <wajihnahle@gmail.com>
2024-02-20 21:17:34 -0700

    init DataAccessException class

---
+++ server/src/main/java/DataAccessException.java
@@ -0,0 +1,10 @@
+// package dataAccess; not sure what this is for
+
+/**
+ * Indicates there was an error connecting to the database
+ */
+public class DataAccessException extends Exception{
+  public DataAccessException(String message) {
+    super(message);
+  }
+}
