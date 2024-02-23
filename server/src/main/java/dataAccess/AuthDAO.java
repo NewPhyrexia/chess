244da534c2de80c8381c8c819822ef30451347f7
wajih <wajihnahle@gmail.com>
2024-02-22 22:05:07 -0700

    finished gameDAO and AuthDAO for clearall  end point prep

--- server/src/main/java/dataAccess/AuthDAO.java
+++ server/src/main/java/dataAccess/AuthDAO.java
@@ -1,5 +1,20 @@
 package dataAccess;
 
-public class AuthDAO {
+import model.AuthData;
 
+import java.util.HashMap;
+
+public class AuthDAO {
+  final private HashMap<String, AuthData> allAuthTokens = new HashMap<>();
+
+  public AuthData addAuthToken(AuthData token) {
+    token = new AuthData(token.authToken(), token.username());
+
+    allAuthTokens.put(token.authToken(), token);
+    return token;
+  }
+
+  public void deleteAllAuthTokens() throws DataAccessException {
+    allAuthTokens.clear();
+  }
 }
--- server/src/main/java/dataAccess/GameDAO.java
+++ server/src/main/java/dataAccess/GameDAO.java
@@ -1,4 +1,22 @@
 package dataAccess;
 
+
+import model.AuthData;
+import model.GameData;
+
+import java.util.HashMap;
+
 public class GameDAO {
+  final private HashMap<Integer, GameData> allGames = new HashMap<>();
+
+  public GameData addGameData(GameData game) {
+    game = new GameData(game.gameID(), game.whiteUsername(), game.blackUsername(), game.gameName(), game.implementation());
+
+    allGames.put(game.gameID(), game);
+    return game;
+  }
+
+  public void deleteAllAuthTokens() throws DataAccessException {
+    allGames.clear();
+  }
 }
