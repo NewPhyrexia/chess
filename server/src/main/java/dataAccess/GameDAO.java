513fdaa9df94dd19ddad3b984e584bc4a6ac1985
wajih <wajihnahle@gmail.com>
2024-02-23 22:41:21 -0700

    finished necessary methods in UserDAO for clear functionality

--- server/src/main/java/dataAccess/GameDAO.java
+++ server/src/main/java/dataAccess/GameDAO.java
@@ -1,7 +1,6 @@
 package dataAccess;
 
 
-import model.AuthData;
 import model.GameData;
 
 import java.util.HashMap;
--- server/src/main/java/dataAccess/UserDAO.java
+++ server/src/main/java/dataAccess/UserDAO.java
@@ -1,4 +1,21 @@
 package dataAccess;
 
+import model.UserData;
+
+import java.util.HashMap;
+
 public class UserDAO {
+
+  final private HashMap<String, UserData> allUserData = new HashMap<>();
+
+  public UserData addUser(UserData user)  {
+    user = new UserData(user.username(), user.password(), user.email());
+
+    allUserData.put(user.username(),user);
+    return user;
+  }
+
+  public void deleteAllUsers() throws DataAccessException {
+    allUserData.clear();
+  }
 }
