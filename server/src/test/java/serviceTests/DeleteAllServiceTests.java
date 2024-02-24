f8dafbd5931ed2fbc819ebf038670c56dc6beb8a
wajih <wajihnahle@gmail.com>
2024-02-24 10:46:46 -0700

    init authService class

--- server/src/test/java/serviceTests/DeleteAllServiceTests.java
+++ server/src/test/java/serviceTests/DeleteAllServiceTests.java
@@ -1,4 +1,18 @@
 package serviceTests;
 
+import dataAccess.DataAccessException;
+import dataAccess.GameDAO;
+import org.junit.jupiter.api.Test;
+import service.DeleteAllService;
+
 public class DeleteAllServiceTests {
+
+  static final DeleteAllService service = new DeleteAllService();
+
+  @Test
+  void deleteAllDB() throws DataAccessException {
+    service.add
+
+    service.deleteAllDB();
+  }
 }
