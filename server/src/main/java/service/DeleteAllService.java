e4016e7601fb839da2cc7ff1bc05d748483fb0eb
wajih <wajihnahle@gmail.com>
2024-02-23 23:41:17 -0700

    fixed the names of the services

--- server/src/main/java/service/DeleteAllService.java
+++ server/src/main/java/service/DeleteAllService.java
@@ -1,7 +1,20 @@
 package service;
 
+import dataAccess.AuthDAOInterface;
+import dataAccess.DataAccessException;
+import dataAccess.GameDAOInterface;
+import dataAccess.UserDAOInterface;
+
+
 public class DeleteAllService {
-//  public AuthData createGame(GameData game) {}
-//  public AuthData joinGame(GameData game) {}
-//  public AuthData listGames(GameData game) {}
+
+  private AuthDAOInterface authInterface;
+  private GameDAOInterface gameInterface;
+  private UserDAOInterface userInterface;
+
+  public void deleteAllDB() throws DataAccessException{
+    authInterface.deleteAllAuthTokens();
+    gameInterface.deleteAllGames();
+    userInterface.deleteAllUsers();
+  }
 }
---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,11 @@
+package service;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+public class GameService {
+//  public AuthData createGame(GameData game) {}
+//  public AuthData joinGame(GameData game) {}
+//  public AuthData listGames(GameData game) {}
+}
