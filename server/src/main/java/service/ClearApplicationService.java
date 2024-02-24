039ca15f85920620ee73df1b0dc3bd4d5982c676
wajih <wajihnahle@gmail.com>
2024-02-23 23:40:12 -0700

    finished delete all service

--- server/src/main/java/service/ClearApplicationService.java
+++ server/src/main/java/service/ClearApplicationService.java
@@ -1,11 +1,22 @@
 package service;
 
+import dataAccess.AuthDAOInterface;
 import dataAccess.DataAccessException;
+import dataAccess.GameDAOInterface;
+import dataAccess.UserDAOInterface;
+
 import java.util.Collection;
 
 
 public class ClearApplicationService {
-  public void deleteAllDB() {
 
+  private AuthDAOInterface authInterface;
+  private GameDAOInterface gameInterface;
+  private UserDAOInterface userInterface;
+
+  public void deleteAllDB() throws DataAccessException{
+    authInterface.deleteAllAuthTokens();
+    gameInterface.deleteAllGames();
+    userInterface.deleteAllUsers();
   }
 }
--- server/src/main/java/service/DeleteAllService.java
+++ server/src/main/java/service/DeleteAllService.java
@@ -1,10 +1,6 @@
 package service;
 
-import model.AuthData;
-import model.GameData;
-import model.UserData;
-
-public class GameService {
+public class DeleteAllService {
 //  public AuthData createGame(GameData game) {}
 //  public AuthData joinGame(GameData game) {}
 //  public AuthData listGames(GameData game) {}
