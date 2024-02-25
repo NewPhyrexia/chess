c53a418adc7b66f1e27c41553f3c80cf5ddad158
wajih <wajihnahle@gmail.com>
2024-02-25 16:03:54 -0700

    Finished negative Register test

--- server/src/test/java/serviceTests/UserServiceTests.java
+++ server/src/test/java/serviceTests/UserServiceTests.java
@@ -25,7 +25,14 @@ public class UserServiceTests {
   }
 
   @Test
-  void failToCreateUser() throws DataAccessException {
+  void duplicateUser() throws DataAccessException {
+    UService.register(new UserData("Dakota", "1sC00l4", "Iam@hotmail.com"));
+    UService.register(new UserData("Callie", "H0tStuff1", "TurtleDuck@gmail.com"));
+    UService.register(new UserData("Anna", "BanANNA77", "IamtheMASTERcommander@gmail.com"));
 
+    // user with same username attempts to create
+    UService.register(new UserData("Dakota", "Fake14", "IamNot@hotmail.com"));
+
+    assertEquals(3, UService.listUsers().size());
   }
 }
