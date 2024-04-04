7425c473d61694a0a87beb921b5252c8e9a71c60
wajih <wajihnahle@gmail.com>
2024-04-04 14:02:29 -0600

    finished connection and connectionManager classes for the time being

--- server/src/main/java/server/webSocket/ConnectionManager.java
+++ server/src/main/java/server/webSocket/ConnectionManager.java
@@ -1,4 +1,37 @@
 package server.webSocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+
+import javax.management.Notification;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ConnectionManager {
+  public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+  public void add(String userName, Session session) {
+    var connection = new Connection(userName, session);
+    connections.put(userName, connection);
+  }
+
+  public void remove(String userName) {connections.remove(userName);}
+
+  public void broadcast(String excludedUser, Notification notification) throws IOException {
+    var removeConnections =  new ArrayList<Connection>();
+    for (var c : connections.values()) {
+      if (c.session.isOpen()) {
+        if (!c.userName.equals(excludedUser)) {
+          c.send(notification.toString());
+        }
+      } else {
+        removeConnections.add(c);
+      }
+    }
+
+    // Close sessions that were left open
+    for (var c : removeConnections) {
+      connections.remove(c.userName);
+    }
+  }
 }
--- server/src/main/java/server/webSocket/WebSocketHandler.java
+++ server/src/main/java/server/webSocket/WebSocketHandler.java
@@ -10,10 +10,36 @@ import java.io.IOException;
 
 @WebSocket
 public class WebSocketHandler {
+
+  private final ConnectionManager connections = new ConnectionManager();
+
   @OnWebSocketMessage
   public void onMessage(Session session, String message) throws IOException {
     System.out.print(message); // for testing
-    var dMessage = new Gson().fromJson(message, UserGameCommand.class);
-    session.getRemote().sendString(message);
+    var userGameCommand = new Gson().fromJson(message, UserGameCommand.class);
+    switch (userGameCommand.getCommandType()) {
+      case LEAVE -> leave();
+      case RESIGN -> resign();
+      case MAKE_MOVE -> makeMove();
+      case JOIN_PLAYER -> joinPlayer();
+      case JOIN_OBSERVER -> joinObserver();
+    }
+    session.getRemote().sendString(message);  // for testing
+  }
+
+  private void leave() throws IOException {
+  }
+
+  private void resign() throws IOException {
+  }
+
+  private void makeMove() throws IOException {
+  }
+
+  private void joinPlayer() throws IOException {
+
+  }
+
+  private void joinObserver() throws IOException {
   }
 }
